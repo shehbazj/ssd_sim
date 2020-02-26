36130356456a18af51a9374ed8aa279090c2bf4c
Shehbaz Jaffer <shehbazjaffer007@gmail.com>
2020-02-25 22:46:46 -0500

    Boost library makefile example

---
+++ boost/build/CMakeFiles/myexec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myexec.dir/test.cpp.o"
+  "myexec.pdb"
+  "myexec"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myexec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
