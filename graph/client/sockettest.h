7634607c8be8df9edfd0290b19e3bec8b34a6d5e
Shehbaz Jaffer <shehbazjaffer007@gmail.com>
2020-03-08 01:05:40 -0500

    client starter

---
+++ graph/client/sockettest.h
@@ -0,0 +1,36 @@
+// myclient.h
+
+#ifndef MYCLIENT_H
+#define MYCLIENT_H
+
+#include <QObject>
+#include <QTcpSocket>
+#include <QDebug>
+#include <QThreadPool>
+#include "mytask.h"
+
+class MyClient : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyClient(QObject *parent = 0);
+    void SetSocket(int Descriptor);
+    virtual ~MyClient() {};
+    
+signals:
+    
+public slots:
+    void connected();
+    void disconnected();
+    void readyRead();
+
+    // make the server fully ascynchronous
+    // by doing time consuming task
+    void TaskResult(int Number);
+
+private:
+    QTcpSocket *socket;
+    
+};
+
+#endif // MYCLIENT_H
