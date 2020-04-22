9b0190174ea88abdb7d52a88d82bab403eb0737a
Andrew Choi <choi@cs.toronto.edu>
2020-04-22 01:48:25 -0400

    Add benchmark code and fix memory leak

--- main_parallel.cpp
+++ main_parallel.cpp
@@ -91,35 +91,28 @@ int single_thread_test() {
 	delete mySSD;
 }
 
-int main(int argc, char* argv[])
-{
-	if (argc <= 2) {
-		cout << "Usage: ./main_parallel [NUM_THREADS] [NUM_BLOCKS]" << endl;
-		return 1;
-	}
-
-	int NUM_THREADS = atoi(argv[1]);
-	int num_blocks = atoi(argv[2]);
-
-	assert(NUM_THREADS > 0);
+int multithread_test(int num_threads, int num_blocks) {
+	assert(num_threads > 0);
 	assert(num_blocks > 0);
+
+	cout << "Threads : " << num_threads << " Blocks: " << num_blocks << endl;
 
 	int block_size = 20; //20 # of pages in block
 	int page_size = 10; //10 # of cells in page
 	int ssd_cell_type = 3; // Multi-level cell
 
-	int blocks_per_thread = num_blocks / NUM_THREADS;
+	int blocks_per_thread = num_blocks / num_threads;
 	int block_capacity = block_size * page_size * ssd_cell_type; // bytes per block
 	int capacity_per_thread = blocks_per_thread * block_capacity; // bytes per thread
 
-	uint8_t** write_buffer = new uint8_t*[NUM_THREADS];
-	uint8_t** read_buffer = new uint8_t*[NUM_THREADS];
+	uint8_t** write_buffer = new uint8_t*[num_threads];
+	uint8_t** read_buffer = new uint8_t*[num_threads];
 
 	ssd *mySSD = new ssd(num_blocks, block_size, page_size, ssd_cell_type);
 	
 
 	// Declare a read buffer with n blocks
-	for (int i = 0 ; i < NUM_THREADS ; i++) {
+	for (int i = 0 ; i < num_threads ; i++) {
 		// write_buffer.push_back(new uint8_t [block_capacity]());
 		read_buffer[i] = (new uint8_t [capacity_per_thread]());
 		write_buffer[i] = (new uint8_t [capacity_per_thread]());
@@ -132,7 +125,7 @@ int main(int argc, char* argv[])
 	// Multithreaded writes to ssd
 	boost::thread_group write_threads;
 	auto write_time = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < NUM_THREADS; ++i) {
+	for (int i = 0; i < num_threads; ++i) {
 		write_threads.create_thread(
 			(boost::bind(&ssd::write_to_disk_threads, mySSD, write_buffer[i], capacity_per_thread, i, blocks_per_thread))
 		);
@@ -145,7 +138,7 @@ int main(int argc, char* argv[])
 	// Multithreaded reads to ssd
 	boost::thread_group read_threads;
 	auto read_start_time = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < NUM_THREADS; ++i) {
+	for (int i = 0; i < num_threads; ++i) {
 		read_threads.create_thread(
 			(boost::bind(&ssd::read_from_disk_threads, mySSD, read_buffer[i], capacity_per_thread, i, blocks_per_thread))
 		);
@@ -154,7 +147,7 @@ int main(int argc, char* argv[])
 	auto end_time = std::chrono::high_resolution_clock::now();
 
 	// Check if read buffer is correct
-	for (int i = 0 ; i < NUM_THREADS ; i++) {
+	for (int i = 0 ; i < num_threads ; i++) {
 		for (int j = 0; j < capacity_per_thread; j++) {
 			// printf("i=%d w=%d r=%d\n", i , write_buffer[i][j], read_buffer[i][j] );
 			assert(write_buffer[i][j] == read_buffer[i][j]);
@@ -164,11 +157,32 @@ int main(int argc, char* argv[])
 	std::cout << "read duration (sec): " << duration / 1000000.0 << endl;
 
 	// Clean up
-	for (int i = 0 ; i < NUM_THREADS ; i++) {
+	for (int i = 0 ; i < num_threads ; i++) {
 		delete []write_buffer[i];
 		delete []read_buffer[i];
 	} 
 
+	delete []write_buffer;
+	delete []read_buffer;
 	delete mySSD;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc <= 2) {
+		cout << "Usage: ./main_parallel [NUM_THREADS] [NUM_BLOCKS]" << endl;
+		return 1;
+	}
+
+	int NUM_THREADS = atoi(argv[1]);
+	int num_blocks = atoi(argv[2]);
+
+	vector<int> threads = {1, 2, 4, 5, 10, 20, 50, 100};
+
+	for (auto i : threads) {
+		multithread_test(i, num_blocks);
+	}
+	
+	return 0;
 }
 
